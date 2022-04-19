621219bbfb3ba5431f56f6e8b4068b79307e0eed
Willian Stephano <will.stephano@gmail.com>
2022-04-19 20:13:48 -0300

    Criação de toggle menu em dispositovos moveis

---
+++ main.js
@@ -0,0 +1,9 @@
+const btToggle = document.querySelector('.bt_toggle');
+const menu = document.querySelector('.menu_nav_cabecalho')
+
+btToggle.addEventListener('click', function () {
+   
+   menu.classList.toggle('menu_nav_cabecalho_ativo')
+
+
+})
