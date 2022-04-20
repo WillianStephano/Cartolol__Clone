6c427fc9be6b10f1ad213950c1fa0411fa4852a2
Willian Stephano <will.stephano@gmail.com>
2022-04-20 10:00:06 -0300

    Correção de erro em toggle menu

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,8 @@
-const btToggle = document.querySelector('.bt_toggle');
+const btToggle = document.querySelector('.bt_cabecalho');
 const menu = document.querySelector('.menu_nav_cabecalho')
 
 btToggle.addEventListener('click', function () {
    
    menu.classList.toggle('menu_nav_cabecalho_ativo')
-
 
 })
